f84f2f8b0f528a703e17fd4fd50d0b3a090fc7ab
zzj <zzj@>
2016-12-12 13:08:19 +0800

    创建OC_LimitedTimeFree项目

---
+++ OC_LimitedTimeFree/OC_LimitedTimeFree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_LimitedTimeFree
+//
+//  Created by zzj on 16/12/12.
+//  Copyright © 2016年 zzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC_LimitedTimeFree/OC_LimitedTimeFree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_LimitedTimeFree
+//
+//  Created by zzj on 16/12/12.
+//  Copyright © 2016年 zzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
